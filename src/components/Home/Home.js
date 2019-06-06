20e58d8cf3429108cd0d2b17042897ef5ec7056d
ZT-User <akhil.chadran@zyxware.com>
2019-06-06 12:34:23 +0530

    initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.scss';
+
+function Home() {
+    return(
+       <section class="home">
+           Home
+       </section>
+    )
+}
+
+export default Home;
